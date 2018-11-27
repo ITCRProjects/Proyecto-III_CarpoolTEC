1a475fc36ef0e6ab62977f20bda0b7b1c6882f69
Ricardo Artavia Solano <37001107+RicardoArtaviaSolano@users.noreply.github.com>
2018-11-26 19:33:00 -0600

    Delete MainServer.java

--- REST_API2/src/java/API/MainServer.java
+++
@@ -1,8 +0,0 @@
-package tec.ac.cr.carpoolingtec.connection;
-
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("carpoolingtec")
-public class MainServer extends Application {}
